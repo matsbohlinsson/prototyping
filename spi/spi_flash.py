c5f7f67b602dc0ab8c0985ab25d7a8a2ea2b7113
Your Name <you@example.com>
2021-10-11 11:56:46 +0200

    spi test works with gpio

--- spi/spi_flash.py
+++ spi/spi_flash.py
@@ -1,31 +1,13 @@
 #!/usr/bin/python3
-
-WREN = 6
-WRDI = 4
-RDSR = 5
-RDSR2 = 0x35
-WRSR = 1
-READ = 3
-WRITE = 2
-SECTOR_ERASE = 0xD8
-# CHIP_ERASE = 0xC7
-CHIP_ERASE = 0xC7
-
-READ_FLAG_STATUS_REGISTER = 0x70
-
-import sys, struct, os, collections
-
-from array import array
-import random
-
+import os
+from functools import lru_cache
 from time import sleep
 import spidev
-from datetime import datetime
-from importlib import reload
 
 def sleep_ms(msecs):
     sleep(float(msecs) / 1000.0)
 
+PAGE_SIZE=256
 
 class spiflash(object):
 
@@ -36,161 +18,149 @@ class spiflash(object):
         self.spi.mode = mode
 
     def __del__(self):
-        try:
-            self.spi.close()
-        except:
-            pass
+        self.spi.close()
 
-    # reads ----------------------------------------------------------------------------------
     def read_id(self):
-        # device_id = self.spi.xfer2([0x9E,0x9F])[1]
-        #AS_CHECK_SILICON_ID			0x9F
-        #device_id = self.spi.xfer2([0x9E, 0x9F, 0])[1:]
         device_id = self.spi.xfer2([0x9F, 0x9F])[1:]
         return device_id
 
-    def read_status(self):
-        statreg = self.spi.xfer2([RDSR, RDSR])[1]
-        # statreg2 = self.spi.xfer2([RDSR2,RDSR2])[1]
-        return statreg
-
-    def read_flag_status(self):
-        statreg = self.spi.xfer2([READ_FLAG_STATUS_REGISTER, READ_FLAG_STATUS_REGISTER])[1]
-        # statreg2 = self.spi.xfer2([RDSR2,RDSR2])[1]
-        return statreg
-
-    def read_page_old(self, byte_1, byte_2):
-        xfer = [READ, 0, byte_2, byte_1] + [255 for _ in range(256)]  # command + 256 dummies
-        return self.spi.xfer2(xfer)[4:]  # skip 4 first bytes (dummies)
-
-    # writes ----------------------------------------------------------------------------------
     def write_enable(self):
-        self.spi.xfer2([WREN])
+        self.spi.xfer2([0x06])
         sleep_ms(5)
-
-    def write_disable(self):
-        self.spi.xfer2([WRDI])
-        sleep_ms(5)
-
-    def write_status(self, s1, s2):
-        self.write_enable()
-
-        self.spi.xfer2([WRSR, s1, s2])
-        sleep_ms(10)
-
-        self.wait_until_not_busy()
-
-    def write_page_old(self, byte_1, byte_2, page):
-        self.write_enable()
-
-        # print self.read_status()
-
-        xfer = [WRITE, 0, byte_2, byte_1] + page[:256]
-        self.spi.xfer2(xfer)
-        sleep_ms(10)
-
-        self.wait_until_not_busy()
-
-    def write_and_verify_page(self, addr1, addr2, page):
-        self.write_page_to_flash(addr1, addr2, page)
-        return self.read_page(addr1, addr2)[:256] == page[:256]
-
-    # erases ----------------------------------------------------------------------------------
-    def erase_sector(self, addr1, addr2):
-        self.write_enable()
-
-        xfer = [SECTOR_ERASE, 0, addr2, addr1]
-        self.spi.xfer2(xfer)
-        sleep_ms(10)
-
-        self.wait_until_not_busy()
 
     def erase_all(self):
         self.write_enable()
-
-        self.spi.xfer2([CHIP_ERASE])
+        self.spi.xfer2([0xC7])
         sleep_ms(10)
-
         self.wait_until_not_busy()
 
-    # misc ----------------------------------------------------------------------------------
     def wait_until_not_busy(self):
-        statreg = 0x1;
-        while (statreg & 0x1) == 0x1:
-            # Wait for the chip.
-            statreg = self.spi.xfer2([RDSR, RDSR])[1]
-            # print "%r \tRead %X" % (datetime.now(), statreg)
+        while (self.spi.xfer2([0x5, 0x5])[1] & 0x1) == 0x1:
             sleep_ms(5)
-
-    # helpers -------------------------------------------------------------------------------
-    def print_status(self, status):
-        print("status %s %s" % (bin(status[1])[2:].zfill(8), bin(status[0])[2:].zfill(8)))
-
-    def print_page(self, page):
-        s = ""
-        for row in range(16):
-            for col in range(16):
-                s += "%02X " % page[row * 16 + col]
-            s += "\n"
-        print(s)
-
-    def fill_memory(self, start_page, stop_page):
-        for page in range(start_page, stop_page):
-            self.write_page_to_flash(page, [x + page for x in range(256)])
 
     def write_page_to_flash(self, page, bytes):
         self.write_enable()
-        self.spi.xfer2([6])  # WREN  Write enable
         self.spi.xfer2([0x2, int(page / 256), page % 256, 0] + bytes)  # Write page 255
         sleep_ms(10)
         self.wait_until_not_busy()
-
-    def read_memory(self, start_page, stop_page):
-        for page in range(start_page, stop_page):
-            print(f'Page:{page}')
-            bytes = self.spi.xfer2([0x3, int(page/256), page%256, 0] + [255 for _ in range(256)])
-            print(f'{bytes}')
 
     def get_page_from_flash(self, page):
         xfer=[0x3, int(page/256), page%256, 0] + [255 for _ in range(256)]
         return self.spi.xfer2(xfer)[4:]  # skip 4 first bytes (dummies)
 
 
-# TESTS -------------------------------------------------------------------
+    def write_pages(self, pages):
+        for page_nbr, page_bytes in enumerate(pages):
+            print(f"Writing page:{page_nbr}", end='\r')
+            self.write_page_to_flash(page_nbr, page_bytes)
+
+    def verify_pages(self, pages):
+        for page_nbr, page_bytes in enumerate(pages):
+            print(f"Verifying page:{page_nbr}", end='\r')
+            bytes_flash = self.get_page_from_flash(page_nbr)
+            if bytes_flash != page_bytes:
+                print("Verify error:")
+                print(bytes_flash)
+                print(page_bytes)
+                exit(1)
+
 
 # 		pCSpiDev = new CSpi("HLD FPGA", "/dev/spidev1.3", cMode, 8, 8000, 0, FALSE, FALSE);		// active low cs
 #       	cMode |= SPI_CPHA; // Phase, data is clocked out on falling_edge and sampled on rising_edge
 # 	cMode |= SPI_CPOL; // Polarity => Clock is default high
 # SPI_MODE_3		(SPI_CPOL|SPI_CPHA)
-chip = spiflash(bus=1, cs=3, mode=3, max_speed_hz=180000)
-print(chip.read_id())
 
-
-PAGE_SIZE=256
-def get_pages_from_file(file_name:str, fill_last_page=0xff):
+def get_pages_from_file(file_name:str, pad_last_page=0xff):
     pages=[]
     with open(file_name, 'rb') as f:
         while (chunk := f.read(PAGE_SIZE)) != b'':
             pages.append(list(chunk))
+    # Fill last page to PAGE_SIZE
     last_page = pages[-1]
     for i in range(PAGE_SIZE - len(last_page)):
-        last_page.append(fill_last_page)
+        last_page.append(pad_last_page)
     return pages
 
+
+def setGPIO(pin, value):
+    if not os.path.exists("/sys/class/gpio/gpio"+str(pin)):
+        os.system('echo '+str(pin)+' > /sys/class/gpio/export')
+        os.system('echo out > /sys/class/gpio/gpio'+str(pin)+'/direction')
+    os.system('echo '+str(value)+ ' > /sys/class/gpio/gpio'+str(pin)+'/value')
+
+
+def getGPIO(pin):
+    if not os.path.exists("/sys/class/gpio/gpio"+str(pin)):
+        os.system('echo '+str(pin)+' > /sys/class/gpio/export')
+        os.system('echo in > /sys/class/gpio/gpio'+str(pin)+'/direction')
+    gpiopin = "gpio%s" % (str(pin), )
+    pin = open("/sys/class/gpio/"+gpiopin+"/value","r")
+    value = pin.read()
+    pin.close()
+    return int(value)
+
+
+@lru_cache(maxsize=None)
+def reverse(byte):
+    return int('{:08b}'.format(byte)[::-1], 2)
+
+def reverse_bits(pages):
+    reversed_pages=[]
+    for page in pages:
+        l = []
+        for byte in page:
+            l.append(reverse(byte))
+        reversed_pages.append(l)
+    return reversed_pages
+
+'''
+setlow /sys/class/gpio/gpio125/value
+setlow /sys/class/gpio/gpio127/value
+Info: File size: 162976 bytes.
+sethigh /sys/class/gpio/gpio125/value
+setlow /sys/class/gpio/gpio127/value
+setlow /sys/class/gpio/gpio125/value
+sethigh /sys/class/gpio/gpio127/value
+'''
+def enable_flash_mode():
+    NCONFIG=125
+    NCE=127
+    setGPIO(NCONFIG, 0)
+    setGPIO(NCE, 0)
+    setGPIO(NCONFIG, 1)
+    setGPIO(NCE, 0)
+    setGPIO(NCONFIG, 0)
+    setGPIO(NCE, 1)
+
+def disable_flash_mode():
+    NCONFIG=125
+    NCE=127
+    setGPIO(NCE, 0)
+    setGPIO(NCONFIG, 1)
+
+
 def test():
+    chip = spiflash(bus=1, cs=3, mode=3, max_speed_hz=12000000)
+    print("ID:", chip.read_id())
+    print("Enable flash mode")
+    enable_flash_mode()
+    sleep_ms(100)
+
+    print("ID:", chip.read_id())
+
     print("Erasing")
     chip.erase_all()
     pages = get_pages_from_file('hld_fpga_0400_0001.rpd')
-    for page_nbr,page_bytes in enumerate(pages):
-        print(f"Writing page:{page_nbr}", end='\r')
-        chip.write_page_to_flash(page_nbr, page_bytes)
+    reverse_pages = reverse_bits(pages)
+    chip.write_pages(reverse_pages)
 
     print('\nVerify!')
-    for page_nbr,page_bytes in enumerate(pages):
-        print(f"Verifying page:{page_nbr}", end='\r')
-        bytes_flash=chip.get_page_from_flash(page_nbr)
-        if bytes_flash!=page_bytes:
-            print("Verify error")
-            exit(1)
+    chip.verify_pages(reverse_pages)
 
+    disable_flash_mode()
+    print("ID:", chip.read_id())
     print('\nSuccess!')
+
+
+
+
